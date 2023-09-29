7dd339449e329c54af00dd044368b873c6ed8a36
foviedo232 <141380909+foviedo232@users.noreply.github.com>
2023-09-29 16:40:17 -0300

    Delete algoa.js

--- algoa.js
+++
@@ -1,5 +0,0 @@
-var baseDatos = [
-    { Articulo: '099882', Descripcion: 'Contrafrente Abisagrado Ciego p/S9000 - 300x300', Medida: '300x300' },
-    { Articulo: '099982', Descripcion: 'Contrafrente Abulonado Ciego p/S9000 - 300x300', Medida: '300x300' },
-    // ... (resto de tus datos)
-];
